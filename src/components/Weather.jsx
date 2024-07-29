53988330e0dc8d9568d98c0b9d69a98eaae59678
Suomi228 <bixep345@gmail.com>
2024-07-29 17:03:29 +0300

    input design

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,9 +2,11 @@ import './Weather.css'
 import search_icon from '../assets/search.png'
 function Weather() {
   return (
-    <div>
-      <input id="search" type="text" placeholder='Search'/>
-      <img src={search_icon} alt=""/>
+    <div className='weather'>
+      <div className='search-bar'>
+        <input id="id" type="text" placeholder='Search'/>
+        <img src={search_icon} alt=""/>
+      </div>
     </div>
   )
 }
