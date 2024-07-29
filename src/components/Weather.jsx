7cc4a39632dbd2469b7ad2f03d965d44e76dacb4
Suomi228 <bixep345@gmail.com>
2024-07-29 19:58:27 +0300

    data fetching

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -8,22 +8,50 @@ import rain_icon from '../assets/rain.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import { useEffect } from 'react'
+import { useState } from 'react'
 import axios from 'axios';
 
 function Weather() {
 
+
+  const [wheatherData, setwheatherData] = useState(false);
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  }
   const search = async (city) => {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`)
-      
-      console.log(response.data)
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`)
+      const data = response.data;
+      console.log(data)
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setwheatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon
+    })
+
     }
     catch (error) {
       console.error('Error fetching weather data:', error)
     }
   }
   useEffect(()=>{
-    search('London') 
+    search('Oslo') 
   },[])
 
   return (
@@ -32,21 +60,21 @@ function Weather() {
         <input id="id" type="text" placeholder='Search'/>
         <img src={search_icon} alt=""/>
       </div>
-      <img src={clear_icon} alt="" className='weather-icon'/>
-      <p className='temperature'>25°C</p>
-      <p className='location'>London</p>
+      <img src={wheatherData.icon} alt="" className='weather-icon'/>
+      <p className='temperature'>{wheatherData.temperature}°C</p>
+      <p className='location'>{wheatherData.location}</p>
       <div className="weather-data">
         <div className="col">
           <img src={humidity_icon} alt="" className='icon'/>
           <div>
-            <p>91%</p>
+            <p>{wheatherData.humidity}%</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="col">
           <img src={wind_icon} alt="" className='icon'/>
           <div>
-            <p>3.6 Km/h</p>
+            <p>{wheatherData.windSpeed} Km/h</p>
             <span>Wind speed</span>
           </div>
         </div>
